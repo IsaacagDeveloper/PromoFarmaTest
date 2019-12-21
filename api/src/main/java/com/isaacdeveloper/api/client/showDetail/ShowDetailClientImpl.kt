ea8c45eb1d9b53a92374a710bdf55020d7f82bf3
Isaac <isaacag.develop@gmail.com>
2019-12-21 17:52:44 +0100

    End clients
    We end the develop of the clients.

--- api/src/main/java/com/isaacdeveloper/api/client/showDetail/ShowDetailClientImpl.kt
+++ api/src/main/java/com/isaacdeveloper/api/client/showDetail/ShowDetailClientImpl.kt
@@ -1,4 +1,28 @@
 package com.isaacdeveloper.api.client.showDetail
 
-class ShowDetailClientImpl {
+import android.util.Log
+import com.isaacdeveloper.api.RetrofitService
+import com.isaacdeveloper.api.model.ResponseState
+import com.isaacdeveloper.api.model.response.showDetail.ShowDetailApiResponseModel
+
+class ShowDetailClientImpl(
+    val retrofitService: RetrofitService
+): ShowDetailClient {
+
+    override fun getShow(query: String): ShowDetailApiResponseModel {
+        var showDetailApiResponseModel = ShowDetailApiResponseModel(null, ResponseState.ERROR)
+        try {
+            val endpoint = retrofitService.service.getShowDetail(query)
+            val apiResponse = endpoint.execute()
+            if (apiResponse.isSuccessful) {
+                apiResponse.body()?.apply {
+                    showDetailApiResponseModel = this
+                    showDetailApiResponseModel.responseState = ResponseState.SUCCESS
+                }
+            }
+        }catch (e: Exception) {
+            Log.e(javaClass.simpleName, e.message.toString())
+        }
+        return showDetailApiResponseModel
+    }
 }
